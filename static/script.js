00c150ffcf9109c8db59cdb602eab91bcab2d7b1
jorge9200 <jorge92f@gmail.com>
2015-06-22 16:13:59 +0200

    Contacts initial version

---
+++ static/script.js
@@ -0,0 +1,7 @@
+'use strict'
+
+// CONSTANTS
+var BROWSER_FIREFOX = 0;
+var BROWSER_IE = 1;
+var BROWSER_WEBKIT = 2;
+var BROWSER_TYPE = /webkit/i.test(navigator.userAgent) ? BROWSER_WEBKIT : (/trident/i.test(navigator.userAgent) ? BROWSER_IE : BROWSER_FIREFOX);
